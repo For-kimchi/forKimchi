fe376f14fef3aafd85efac6e33c656c0e7ca6c19
youhwan <qjdxjd0643@naver.com>
2025-04-27 17:46:00 +0900

    처음시도

--- for-kimchi-client/src/router/index.js
+++ for-kimchi-client/src/router/index.js
@@ -2,8 +2,12 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import Tables from "../views/Tables.vue";
 import Notifications from "../views/Notifications.vue";
-
+import Productionplan from "../views/Productionplan.vue";
+import Productionorder from "../views/Productionorder.vue";
+import Productionprocess from "../views/Productionprocess.vue";
+import Productionprocwork from "../views/Productionprocwork.vue";
 const routes = [
+  // 예시
   {
     path: "/",
     name: "/",
@@ -18,7 +22,28 @@ const routes = [
     path: "/notifications",
     name: "Notifications",
     component: Notifications,
-  }
+  },
+  // 유환
+  {
+    path: "/Productionplan",
+    name: "Production plan",
+    component: Productionplan,
+  },
+  {
+    path: "/Productionorder",
+    name: "Production order",
+    component: Productionorder,
+  },
+  {
+    path: "/Productionprocess",
+    name: "Production process",
+    component: Productionprocess,
+  },
+  {
+    path: "/Productionprocwork",
+    name: "Production procwork",
+    component: Productionprocwork,
+  },
 ];
 
 const router = createRouter({
---
+++ for-kimchi-client/src/views/Productionprocess.vue
@@ -0,0 +1,19 @@
+<template>
+    <table>
+        <tr>
+            <td>dd</td>
+            <td>dd</td>
+            <td>dd</td>
+        </tr>
+    </table>
+    </template>
+    
+    <script>
+    import axios from 'axios'
+    
+    export default {
+        name: "Production process",
+        
+    }
+    
+    </script>
