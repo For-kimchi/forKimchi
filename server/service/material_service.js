9734a754d99d75f458618ea91a4adcb338a86e74
eohhh <asas0066@naver.com>
2025-04-29 17:37:38 +0900

    250429

--- server/service/material_service.js
+++ server/service/material_service.js
@@ -1,10 +1,19 @@
-const mariaDB = require('../mapper/mapper');
+const mariaDB = require('../mapper/mapper.js');
+const { convertObjToQuery } = require('../utils/converts');
 
-const mateReqAll = async() => {
-  let list = await mariaDB.query('selectMateReq');
+const mateReqAll = async(searchList) => {
+  let searchKeyword = Object.keys(searchList).length > 0 ? convertObjToQuery(searchList) : '';
+  let list = await mariaDB.query('selectMateReq', searchKeyword);
   return list;
 };
 
+const  mateReqById = async(mateNo) => {
+  let list = await mariaDB.query('selectMateDetail', mateNo);
+  return list;
+};
+
+
 module.exports = {
   mateReqAll,
+  mateReqById,
 }
