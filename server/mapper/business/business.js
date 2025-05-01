b1a3d982993e3eb6a4b80987e5bfb1f841abc1dd
kimbab90 <kimbab90@naver.com>
2025-05-01 17:07:04 +0900

    20250501_3

--- server/mapper/business/business.js
+++ server/mapper/business/business.js
@@ -37,9 +37,9 @@ const selectDelivTarget =
  ORD.vendor_id,
  VE.vendor_name,
  PR.prod_name,
- order_amount,
- deliv_due_date,
- order_status,
+ ORDD.order_amount,
+ ORDD.deliv_due_date,
+ ORDD.order_status,
  ORDD.memo
 FROM t_order_detail ORDD
 JOIN t_order ORD ON ORDD.order_id = ORD.order_id
@@ -48,10 +48,22 @@ JOIN t_vendor VE ON ORD.vendor_id = VE.vendor_id
 AND deliv_due_date BETWEEN ? AND ?
 ORDER BY deliv_due_date 
 `;
-
-
-
 //WHERE order_status = '4z' 나중에 데이터 있으면 필요한 조건
+
+const selectDelivProdTarget =
+  `SELECT PW.prod_lot,
+  PW.prod_id,
+  PR.prod_name,
+  WA.warehouse_id,
+  WA.warehouse_name,
+  PW.prod_amount,
+  PW.expired_date
+  FROM t_prod_warehouse PW
+  JOIN t_prod PR ON PW.prod_id = PR.prod_id
+  JOIN t_warehouse WA ON PW.warehouse_id = WA.warehouse_id
+  WHERE lot_status <> '1aa'
+`;
+//기존에 납품된 수량으로 입고수량과 비교해야 함
 
 module.exports = {
   selectLastOrder,
@@ -59,4 +71,5 @@ module.exports = {
   insertOrder,
   insertOrderDetail,
   selectDelivTarget,
+  selectDelivProdTarget,
 }
