977c0fb178a0af42a9f696864706f82307d61ba6
youhwan <qjdxjd0643@naver.com>
2025-05-09 17:34:03 +0900

    학원 오후

--- server/mapper/sqlList.js
+++ server/mapper/sqlList.js
@@ -16,6 +16,7 @@ const deliv = require('./business/deliv.js');
 // 생산
 const plan = require('./production/prodPlan.js');
 const prodOrder = require('./production/prodOrder.js');
+const prodProcess = require('./production/prodProcess.js');
 // 검사
 const quality = require('./quality/quality.js');
 module.exports = {
@@ -39,6 +40,7 @@ module.exports = {
     // 생산
     ...plan,
     ...prodOrder,
+    ...prodProcess,
     //검사
     ...quality,
 }
